2b215ed3b928f6a632bb8fb0027b6fb6f93f8b60
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-11-06 17:21:45 -0400

    fixed content overflow

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,19 +1,29 @@
 import Messages from "./Mesages";
+import { Form } from "formik";
+import "../styles/card.css";
 
 const Card = () => {
     return (
-        <div className="content flex mt-8 flex-col relative w-1/2 h-fit bg-white">
+        <div className="content flex my-8 flex-col relative w-1/2 bg-white">
             <div className="header bg-darkPurple flex justify-center font-light text-white text-base p-2
             rounded-t-lg">
                 <h1>Created by <span className="text-lightPurple">Angelo Martínez</span></h1>
             </div>
-            <div className="chat">
-                <h1 className="title font-medium text-3xl p-4">Mini Message Board</h1>
+            <h1 className="title font-medium text-3xl p-4">Mini Message Board</h1>
+            <div className="chat p-2 flex flex-col overflow-y-auto">
+                <Messages />            
+            </div>
+            
+            <div className="form">
+                <Form>
+
+                </Form>
             </div>
 
-            <Messages />
+            
 
         </div>
+        /* create user input */
     )
 }
 
--- client/src/components/Mesages.jsx
+++ client/src/components/Mesages.jsx
@@ -5,9 +5,15 @@ const Messages = () => {
         <>
         {placeholder.map((item) => (
             <div key={item.id} className="px-4">
-                <h1 className="mb-2 font-medium text-xl">{item.name}</h1>
-                <h1 className="mb-8 bg-green-200 text-lg w-fit p-4 rounded">{item.text}</h1>
+                <div className="message-header flex items-center justify-between">
+                    <h1 className="mb-2 font-medium text-xl break-all">{item.name}</h1>
+                    <h1 className="text-slate-500 ml-2">{item.date}</h1>
+                </div>
+                <div className="w-fit">
+                    <h1 className="mb-8 bg-green-200 text-lg break-all p-4 rounded">{item.text}</h1>
+                </div>
             </div>
+            /* Terminar front-end */
         ))}
         </>
     )
